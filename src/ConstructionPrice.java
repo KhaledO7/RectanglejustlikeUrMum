5829eb4a6848d1cf3fc499b0356270d581cd7eee
KhaledO7 <khaledo@nycstudents.net>
2024-10-07 13:19:42 -0400

    ur gae

---
+++ src/ConstructionPrice.java
@@ -0,0 +1,16 @@
+public class ConstructionPrice {
+    private double boardPrice;
+    private double windowPrice;
+    private double taxRate;
+
+
+    public ConstructionPrice(double boardPrice, double windowPrice, double taxRate)
+    {
+        this.boardPrice = boardPrice;
+        this.windowPrice = windowPrice;
+        this.taxRate = taxRate;
+
+
+
+    }
+}
