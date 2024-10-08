c8a010a47c94bef213753e10f71817ffe4b5a2d0
BT_3W2_26 <khaledo@nycstudents.net>
2024-10-08 13:14:43 -0400

    im finally finished after hours of time of my life

--- src/Game.java
+++ src/Game.java
@@ -18,15 +18,30 @@ public class Game
 
     /** Add the getScore() "getter" method here */
     /* to be completed in part (a) */
+    public int getScore(){
+        return score;
+    }
 
     /** Add the addPlayer() method here */
     /* to be completed in part (b) */
+    public int addPlayer(){
+        players++;
+        return players;
+    }
 
     /** Add the increaseScore(int increase) method here */
     /* to be completed in part (c) */
+    public int increaseScore(int oh){
+        score += oh;
+        return score;
+    }
 
     /** Add the averageScorePerPlayer() method here */
     /* to be completed in part (d) */
+    public int averageScorePerPlayer(){
+        int avg = score/players;
+        return avg;
+    }
 
     public void end()
     {
--- src/Main.java
+++ src/Main.java
@@ -7,39 +7,16 @@ public class Main {
     public static void main(String[]args){
 
 
-        double length, width, height;
+        Game game = new Game();
+        game.addPlayer();
+        game.addPlayer();
+        game.addPlayer();
+        game.increaseScore(8);
+        game.increaseScore(3);
+        System.out.println("Players: " + game.getPlayers());
+        System.out.println("Score: " + game.getScore());
+        System.out.println("Avg score per player: " + game.averageScorePerPlayer());
 
-        length = 5.0;
-        width = 10.0;
-        height = length;
-
-        Box box1 = new Box(length, width, height);
-        Box box2 = new Box(length);
-        box1.printDimensions();
-        box2.printDimensions();
-
-        String bigger;
-
-        if (box1.volume() > box2.volume())
-        {
-            bigger = "box1 has greater volume";
-        }
-        else if (box1.volume() < box2.volume())
-        {
-            bigger = "box2 has greater volume";
-        }
-        else
-        {
-            bigger = "both boxes have equal volume";
-        }
-
-        System.out.println(bigger);
-
-        boolean box1sides = box1.anySideLongerThan(length);
-        boolean box2sides = box2.anySideLongerThan(length);
-
-        System.out.println("box1 has a side that exceeds " + length + ": " + box1sides);
-        System.out.println("box2 has a side that exceeds " + length + ": " + box2sides);
 
 
 
