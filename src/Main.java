f0ada5256f10afeedf7c9445225c4826ec69debb
BT_3W2_26 <khaledo@nycstudents.net>
2024-10-09 13:21:24 -0400

    ez

--- src/Main.java
+++ src/Main.java
@@ -7,15 +7,14 @@ public class Main {
     public static void main(String[]args){
 
 
-        Game game = new Game();
-        game.addPlayer();
-        game.addPlayer();
-        game.addPlayer();
-        game.increaseScore(8);
-        game.increaseScore(3);
-        System.out.println("Players: " + game.getPlayers());
-        System.out.println("Score: " + game.getScore());
-        System.out.println("Avg score per player: " + game.averageScorePerPlayer());
+        Rectangle r1 = new Rectangle();
+        Rectangle r2 = r1;
+        r1 = null;
+        r1.printRectangleInfo();
+
+
+
+
 
 
 
--- src/Rectangle.java
+++ src/Rectangle.java
@@ -6,16 +6,10 @@ public class Rectangle
     private int length;
     private int width;
 
-
-
-
-  /* Below are the Rectangle class "constructor" methods;
-      this class has THREE constructors! (we will learn later about
-      why this might be useful).  Each of these creates a Rectangle object.
-  */
-
-
-
+   /* Below are the Rectangle class "constructor" methods;
+       this class has THREE constructors! (we will learn later about
+       why this might be useful).  Each of these creates a Rectangle object.
+   */
 
     // This is the constructor to create a Rectangle object with a particular width and length
     public Rectangle(int rectLength, int rectWidth)
@@ -31,9 +25,6 @@ public class Rectangle
         width = side;
     }
 
-
-
-
     // This is the "no-argument constructor" which creates a Rectangle object with default values
     public Rectangle()
     {
@@ -41,18 +32,12 @@ public class Rectangle
         width = 50;
     }
 
-
-
-
-  /* Below are the Rectangle class "instance methods": these are the "behaviors"
-      that a Rectangle object can "do" (or have done to it). Some of these methods
-      return a value (the type of the return value is listed to the right of "public"),
-      while others don't (those that don't have "void" as the return type).
-      Some of these methods have parameters -- something inside the () -- while others don't!
-  */
-
-
-
+   /* Below are the Rectangle class "instance methods": these are the "behaviors"
+       that a Rectangle object can "do" (or have done to it). Some of these methods
+       return a value (the type of the return value is listed to the right of "public"),
+       while others don't (those that don't have "void" as the return type).
+       Some of these methods have parameters -- something inside the () -- while others don't!
+   */
 
     // Returns the length of the Rectangle (a "getter" method)
     public int getLength()
@@ -60,17 +45,11 @@ public class Rectangle
         return length;
     }
 
-
-
-
     // Sets the length of the Rectangle to newLength ("setter" method)
     public void setLength(int newLength)
     {
         length = newLength;
     }
-
-
-
 
     // Returns the width of the Rectangle ("getter" method)
     public int getWidth()
@@ -78,17 +57,11 @@ public class Rectangle
         return width;
     }
 
-
-
-
     // Sets the width of the Rectangle to newWidth ("setter" method)
     public void setWidth(int newWidth)
     {
         width = newWidth;
     }
-
-
-
 
     // Calculates and returns the area of the Rectangle
     public int calculateArea()
@@ -97,23 +70,11 @@ public class Rectangle
         return area;
     }
 
-
-    public int calculatePerimeter()
-    {
-        int perimeter = 2*width + 2*length;
-        return perimeter;
-    }
-
-
-
-
     // Prints the Rectangle's dimensions
     public void printRectangleInfo()
     {
-        System.out.println("This rectangle's length is " + length + " sq ft" + " and width is " + width + " sq ft " + ".");
+        System.out.println("This rectangle's length is " + length + " and width is " + width + ".");
         System.out.println("It has an area of " + calculateArea());
-        System.out.println("It has a perimeter of " + calculatePerimeter());
-
     }
 }
 
